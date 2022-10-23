326ae041128bb30aeea3079aaefc238a3b0f2af7
ArtemShchuchev <shuaa@rambler.ru>
2022-10-23 20:00:41 +0700

    add other files

---
+++ MyProgramm/MyProgramm.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "../DynamicLibrory/Leaver.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");   // задаём русский текст
+	system("chcp 1251");            // настраиваем кодировку консоли
+	std::system("cls");
+
+	std::cout << "Задача 3. Динамическая библиотека CMake\n";
+	std::cout << "---------------------------------------\n" << std::endl;
+	Leaver lv;
+	std::string userName;
+	std::cout << "Введите имя: "; std::cin >> userName;
+	std::cout << lv.leave(userName) << std::endl;
+
+	system("pause"); // Команда задержки экрана
+
+	return 0;
+}
