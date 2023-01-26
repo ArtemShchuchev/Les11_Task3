6be40b238667ce93f62fc6c289b23dc85f69c5fe
ArtemShchuchev <shuaa@rambler.ru>
2023-01-26 23:16:26 +0700

    переделал под линукс

--- DynamicLibrory/Leaver.h
+++ DynamicLibrory/Leaver.h
@@ -1,16 +1,26 @@
 ﻿#pragma once
 #include <iostream>
 
+#if __GNUC__ >= 4
+
+#define EXPORT __attribute__((visibility("default")))
+#define IMPORT __attribute__((visibility("hidden")))
+
+#else
+
 #ifdef DynamicLibrory_EXPORTS
 #define LEAVERDYNAMIC_API __declspec(dllexport)
 #else
 #define LEAVERDYNAMIC_API __declspec(dllimport)
 #endif
 
+#endif
+
+
 class Leaver
 {
 private:
 	const std::wstring LEAVE_STR = L"До свидания, ";
 public:
-	LEAVERDYNAMIC_API std::wstring leave(std::wstring&);
+	EXPORT std::wstring leave(std::wstring&);
 };
--- MyProgramm/MyProgramm.cpp
+++ MyProgramm/MyProgramm.cpp
@@ -2,18 +2,18 @@
 #include "../DynamicLibrory/Leaver.h"
 
 //#include <Windows.h>
-#include <io.h>
-#include <fcntl.h>
+//#include <io.h>
+//#include <fcntl.h>
 
 int main()
 {
-	int err = _setmode(_fileno(stdout), _O_U16TEXT);
-	err = _setmode(_fileno(stdin), _O_U16TEXT);
-	err = _setmode(_fileno(stderr), _O_U16TEXT);
+	//int err = _setmode(_fileno(stdout), _O_U16TEXT);
+	//err = _setmode(_fileno(stdin), _O_U16TEXT);
+	//err = _setmode(_fileno(stderr), _O_U16TEXT);
 
 	//system("chcp 1251");				// настраиваем кодировку консоли
 	
-	//setlocale(LC_ALL, "ru_RU.utf8");
+	setlocale(LC_ALL, "ru_RU.utf8");
 	//setlocale(LC_ALL, "ru_RU");		// задаем русский текст
 	//_wsetlocale(LC_ALL, L"ru_RU");
 	//system("chcp UTF-8");				// настраиваем кодировку консоли
@@ -22,7 +22,7 @@ int main()
 	//SetConsoleCP(CP_UTF8);
 	//SetConsoleOutputCP(CP_UTF8);
 
-	std::system("cls");
+	std::system("clear");
 	std::wcout << L"Задача 3. Динамическая библиотека CMake\n";
 	std::wcout << L"---------------------------------------\n" << std::endl;
 	Leaver lv;
