58db3785be29b5220fb68887b446ce7760c41bb6
ArtemShchuchev <shuaa@rambler.ru>
2023-02-19 18:58:28 +0700

    include new library

--- MyProgramm/MyProgramm.cpp
+++ MyProgramm/MyProgramm.cpp
@@ -1,43 +1,17 @@
 ﻿#include <iostream>
+#include "SecondaryFunction.h"
 #include "../DynamicLibrory/Leaver.h"
-
-//#include <Windows.h>
-#ifdef _WIN32
-#include <io.h>
-#include <fcntl.h>
-#endif
 
 int main()
 {
-#ifdef _WIN32
-	int err = _setmode(_fileno(stdout), _O_U16TEXT);
-	err = _setmode(_fileno(stdin), _O_U16TEXT);
-	err = _setmode(_fileno(stderr), _O_U16TEXT);
-	std::system("cls");
-#else
-	setlocale(LC_ALL, "ru_RU.utf8");
-	std::system("clear");
-#endif
-
+	printHeader(L"Задача 3. Динамическая библиотека CMake");
 	
-	//setlocale(LC_ALL, "ru_RU");		// задаем русский текст
-	//_wsetlocale(LC_ALL, L"ru_RU");
-	//system("chcp UTF-8");				// настраиваем кодировку консоли
-	//system("chcp ANSI");				// настраиваем кодировку консоли
-	//system("chcp 1251");				// настраиваем кодировку консоли
-	//SetConsoleCP(CP_UTF8);
-	//SetConsoleOutputCP(CP_UTF8);
-
-	std::wcout << L"Задача 3. Динамическая библиотека CMake\n";
-	std::wcout << L"---------------------------------------\n" << std::endl;
 	Leaver lv;
 	std::wstring userName;
 	std::wcout << L"Введите имя: "; std::wcin >> userName;
 	std::wcout << userName << std::endl;
 	std::wcout << userName.length() << std::endl;
 	std::wcout << lv.leave(userName) << std::endl;
-
-	//system("pause"); // Команда задержки экрана
 
 	return 0;
 }
